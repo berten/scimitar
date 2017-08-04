6d0fe384f8f19436d94c19cc02adacf2e4f9b51d
Berten <berten@gmail.com>
2017-08-05 00:12:52 +0200

    Remove Useless Stuff from SlackBot

--- slack/src/main/java/be/deschutter/SlackBot.java
+++ slack/src/main/java/be/deschutter/SlackBot.java
@@ -1,6 +1,5 @@
 package be.deschutter;
 
-import be.deschutter.scimitar.events.ReturnType;
 import me.ramswaroop.jbot.core.slack.Bot;
 import me.ramswaroop.jbot.core.slack.Controller;
 import me.ramswaroop.jbot.core.slack.EventType;
@@ -99,94 +98,6 @@ public class SlackBot extends Bot {
         RestTemplate restTemplate = new RestTemplate();
         be.deschutter.scimitar.events.Event reply = restTemplate.postForObject("http://localhost:8080/scimitar", eventFactory.makeEvent(event), be.deschutter.scimitar.events.Event.class);
         reply(session, reply);
-    }
-
-    /**
-     * Invoked when an item is pinned in the channel.
-     *
-     * @param session
-     * @param event
-     */
-    @Controller(events = EventType.PIN_ADDED)
-    public void onPinAdded(WebSocketSession session, Event event) {
-        reply(session, event, new Message("Thanks for the pin! You can find all pinned items under channel details."));
-    }
-
-    /**
-     * Invoked when bot receives an event of type file shared.
-     * NOTE: You can't reply to this event as slack doesn't send
-     * a channel id for this event type. You can learn more about
-     * <a href="https://api.slack.com/events/file_shared">file_shared</a>
-     * event from Slack's Api documentation.
-     *
-     * @param session
-     * @param event
-     */
-    @Controller(events = EventType.FILE_SHARED)
-    public void onFileShared(WebSocketSession session, Event event) {
-        logger.info("File shared: {}", event);
-    }
-
-
-    /**
-     * Conversation feature of JBot. This method is the starting point of the conversation (as it
-     * calls {@link Bot#startConversation(Event, String)} within it. You can chain methods which will be invoked
-     * one after the other leading to a conversation. You can chain methods with {@link Controller#next()} by
-     * specifying the method name to chain with.
-     *
-     * @param session
-     * @param event
-     */
-    @Controller(pattern = "(setup meeting)", next = "confirmTiming")
-    public void setupMeeting(WebSocketSession session, Event event) {
-        startConversation(event, "confirmTiming");   // start conversation
-        reply(session, event, new Message("Cool! At what time (ex. 15:30) do you want me to set up the meeting?"));
-    }
-
-    /**
-     * This method is chained with {@link SlackBot#setupMeeting(WebSocketSession, Event)}.
-     *
-     * @param session
-     * @param event
-     */
-    @Controller(next = "askTimeForMeeting")
-    public void confirmTiming(WebSocketSession session, Event event) {
-        reply(session, event, new Message("Your meeting is set at " + event.getText() +
-                ". Would you like to repeat it tomorrow?"));
-        nextConversation(event);    // jump to next question in conversation
-    }
-
-    /**
-     * This method is chained with {@link SlackBot#confirmTiming(WebSocketSession, Event)}.
-     *
-     * @param session
-     * @param event
-     */
-    @Controller(next = "askWhetherToRepeat")
-    public void askTimeForMeeting(WebSocketSession session, Event event) {
-        if (event.getText().contains("yes")) {
-            reply(session, event, new Message("Okay. Would you like me to set a reminder for you?"));
-            nextConversation(event);    // jump to next question in conversation  
-        } else {
-            reply(session, event, new Message("No problem. You can always schedule one with 'setup meeting' command."));
-            stopConversation(event);    // stop conversation only if user says no
-        }
-    }
-
-    /**
-     * This method is chained with {@link SlackBot#askTimeForMeeting(WebSocketSession, Event)}.
-     *
-     * @param session
-     * @param event
-     */
-    @Controller
-    public void askWhetherToRepeat(WebSocketSession session, Event event) {
-        if (event.getText().contains("yes")) {
-            reply(session, event, new Message("Great! I will remind you tomorrow before the meeting."));
-        } else {
-            reply(session, event, new Message("Oh! my boss is smart enough to remind himself :)"));
-        }
-        stopConversation(event);    // stop conversation
     }
 
 
