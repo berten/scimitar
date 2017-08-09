bb49522644d7da683eaac58d92fa0d350d9bc9e1
berten <berten@gmail.com>
2017-08-09 12:53:25 +0200

    Lookup Test

--- core/src/main/java/be/deschutter/scimitar/planetarion/LookupListener.java
+++ core/src/main/java/be/deschutter/scimitar/planetarion/LookupListener.java
@@ -7,15 +7,20 @@ import be.deschutter.scimitar.PlanetEao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import static java.lang.String.format;
+
 @Component
 public class LookupListener implements Listener {
 
-    @Autowired
-    private PlanetEao planetEao;
+    private final PlanetEao planetEao;
+    private final GalaxyEao galaxyEao;
 
     @Autowired
-    private GalaxyEao galaxyEao;
-
+    public LookupListener(final PlanetEao planetEao,
+        final GalaxyEao galaxyEao) {
+        this.planetEao = planetEao;
+        this.galaxyEao = galaxyEao;
+    }
 
     @Override
     public String getCommand() {
@@ -30,24 +35,49 @@ public class LookupListener implements Listener {
     @Override
     public String getResult(String... parameters) {
         if (parameters != null && parameters.length == 2) {
-            int x = Integer.parseInt(parameters[0]);
-            int y = Integer.parseInt(parameters[1]);
-            Galaxy g = galaxyEao.findFirstByXAndYOrderByTickDesc(x, y);
-            if(g == null)
-                return "Galaxy " + x + ":" + y + " does not exist";
-            return g.getX() + ":" + g.getY() + " '" + g.getGalaxyName() + "' (" + g.getPlanets() + ") Score: " + g.getScore() + " (" + g.getScoreRank() + ") Value: " + g.getValue() + " (" + g.getValueRank() + ") Size: " + g.getSize() + " (" + g.getSizeRank() + ") XP: " + g.getXp() + " (" + g.getXpRank() + ")";
+            try {
+                int x = Integer.parseInt(parameters[0]);
+                int y = Integer.parseInt(parameters[1]);
+
+                Galaxy g = galaxyEao.findFirstByXAndYOrderByTickDesc(x, y);
+                if (g == null)
+                    return "Galaxy " + x + ":" + y + " does not exist";
+                return format(
+                    "%d:%d '%s' (%d) Score: %d (%d) Value: %d (%d) Size: %d (%d) XP: %d (%d)",
+                    g.getX(), g.getY(), g.getGalaxyName(), g.getPlanets(),
+                    g.getScore(), g.getScoreRank(), g.getValue(),
+                    g.getValueRank(), g.getSize(), g.getSizeRank(), g.getXp(),
+                    g.getXpRank());
+            } catch (NumberFormatException e) {
+                return getErrorMessage();
+            }
         }
         if (parameters != null && parameters.length == 3) {
-            int x = Integer.parseInt(parameters[0]);
-            int y = Integer.parseInt(parameters[1]);
-            int z = Integer.parseInt(parameters[2]);
-            Planet p = planetEao.findFirstByXAndYAndZOrderByTickDesc(x,y,z);
-            if(p == null)
-                return "Planet " + x + ":" + y + ":" + z + " does not exist";
-            return p.getX() + ":" + p.getY() + ":" + p.getZ() +" '" + p.getRulerName() + " of " + p.getPlanetName() + "' Score: " + p.getScore() + " (" + p.getScoreRank() + ") Value: " + p.getValue() + " (" + p.getValueRank() + ") Size: " + p.getSize() + " (" + p.getSizeRank() + ") XP: " + p.getXp() + " (" + p.getXpRank() + ")";
+            try {
+                int x = Integer.parseInt(parameters[0]);
+                int y = Integer.parseInt(parameters[1]);
+                int z = Integer.parseInt(parameters[2]);
+                Planet p = planetEao
+                    .findFirstByXAndYAndZOrderByTickDesc(x, y, z);
+                if (p == null)
+                    return "Planet " + x + ":" + y + ":" + z
+                        + " does not exist";
+                return format(
+                    "%d:%d:%d (%s) '%s of %s' Score: %d (%d) Value: %d (%d) Size: %d (%d) XP: %d (%d)",
+                    p.getX(), p.getY(), p.getZ(), p.getRace(), p.getRulerName(),
+                    p.getPlanetName(), p.getScore(), p.getScoreRank(),
+                    p.getValue(), p.getValueRank(), p.getSize(),
+                    p.getSizeRank(), p.getXp(), p.getXpRank());
+            } catch (NumberFormatException e) {
+                return getErrorMessage();
+            }
         } else
-            return "Error: use following pattern for command " + getCommand() + ": " + getPattern();
+            return getErrorMessage();
     }
 
+    private String getErrorMessage() {
+        return "Error: use following pattern for command " + getCommand() + ": "
+            + getPattern();
+    }
 
 }
